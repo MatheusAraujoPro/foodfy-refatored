bcb7d15514882feddf03d04e17bd37a9ae4c71e1
Matheus Araujo <matheusnewff@gmail.com>
2020-07-07 20:24:06 -0300

    Script do modal adicionado

---
+++ src/public/scripts.js
@@ -0,0 +1,18 @@
+const modalOverlay   = document.querySelector('.modal__overlay');
+const cardCollection = document.querySelectorAll('.card');
+const btnClose       = modalOverlay.querySelector('.close__modal');
+const cardContent    = modalOverlay.querySelector('.modal__content');
+
+
+for (let card of cardCollection){
+    card.addEventListener('click' , ()=>{
+        //let cardId = card.getAttribute('id');        
+        modalOverlay.classList.add('actived');
+
+        card.classList.remove('card');
+        card.classList.add('modal__content')
+
+        cardContent.append(card);        
+    })
+}
+
